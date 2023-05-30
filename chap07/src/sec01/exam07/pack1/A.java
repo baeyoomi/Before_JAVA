689651b5a2ca9e834fbd2ea80cd7316dd27cfe5f
baeyoomi <cxz3930@gmail.com>
2023-05-30 18:23:38 +0900

    master2

---
+++ chap07/src/sec01/exam07/pack1/A.java
@@ -0,0 +1,11 @@
+package sec01.exam07.pack1;
+//접근제한자
+public class A {
+	protected String field;
+	
+	protected A() {		
+	}
+	
+	protected void method() {		
+	}
+}
---
+++ chap07/src/sec01/exam07/pack2/C.java
@@ -0,0 +1,12 @@
+package sec01.exam07.pack2;
+
+import sec01.exam07.pack1.A;
+
+public class C {
+	public void method() {
+		// A a = new A();
+		// a.field = "value";
+		// a.method();              -protected 라서 적용안됌 D클래스에서 extends 부모에게 상속받아보자
+		
+	}
+}
